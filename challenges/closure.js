ec4b11089998a8ce6ed9b919ef8240e4ead7ca1a
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2020-09-18 10:24:55 -0600

    completes array-callbacks, prototypes, and classes tasks

--- challenges/closure.js
+++ challenges/closure.js
@@ -1,7 +1,6 @@
-// ==== Closures ==== 
+// ==== Closures ====
 
 /* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
-
 
 const external = "I'm outside the function";
 
@@ -11,14 +10,22 @@ function myFunction() {
 
   function nestedFunction() {
     console.log(internal);
-  };
+  }
   nestedFunction();
 }
 myFunction();
 
-// Explanation: 
-
+// Explanation: nestedFunction has closure from myFunction and can access the variable internal because the variable internal belongs to myFunction. When you console.log internal from nestedFunction being invoked, it looks first inside itself to see if it has that variable, and then outwards to myFunction to see if the variable internal is there to be able to execute the code.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(number) {
+  for (let i = number; i > 0; i++) {
+    if (number > 0) {
+      return i + (i - 1);
+    }
+  }
+}
+console.log(summation(4));
